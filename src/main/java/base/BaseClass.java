2a832abe5cb0f1bcb78d0930da7dfbcca3939998
chiyeti kanchana <kanchanachiyeti333@gmail.com>
2026-03-06 16:10:41 +0530

    Fixed TestNG import issue

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -1,9 +1,11 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 
 
 public class BaseClass {
